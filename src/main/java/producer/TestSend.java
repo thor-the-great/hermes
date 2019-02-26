1cfce663368a499e17599a86f9ec4a0e29a4187c
Unknown <gthor10@gmail.com>
2019-02-25 17:09:40 -0800

    Initial commit

---
+++ src/main/java/producer/TestSend.java
@@ -0,0 +1,21 @@
+package producer;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class TestSend {
+    private final static String QUEUE_NAME = "test-queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
